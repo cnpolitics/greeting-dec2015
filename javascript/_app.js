d02ffbda77d2bea2c4640a9f98b3715dfc301526
Zhengyu Qian <laerhsif@gmail.com>
2015-12-24 22:11:47 +0800

    Init.

---
+++ javascript/_app.js
@@ -0,0 +1,9 @@
+/**
+ * Assembly list:
+ * 1. main.js
+ * 2. sns-share.js
+ * 3. template.js
+ * 4. url-parser.js
+ * 5. wechat-query.js
+ * 6. wechat-result.js
+ */
---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+/* global $, FastClick, Han */
+
+(function() {
+	'use strict';
+	
+	/**
+	 * FastClick configuration
+	 */
+	$(function() {
+		FastClick.attach(document.body);
+	});
+	
+})();
