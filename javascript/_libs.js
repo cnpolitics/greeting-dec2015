fb433a6bea2f91b0514dc9b75df6bc31f5329fd5
Zhengyu Qian <laerhsif@gmail.com>
2015-12-25 02:23:36 +0800

    Create `gh-pages`.

---
+++ javascript/_libs.js
@@ -0,0 +1,6 @@
+/**
+ * @license
+ * jQuery v2.1.4 | (c) 2005, 2015 jQuery Foundation, Inc. | jquery.org/license
+ * doT.js | (c) Laura Doktorova https://github.com/olado/doT | MIT License
+ * FastClick | ftlabs-jsv2 | (c) The Financial Times Limited | MIT License
+ */
